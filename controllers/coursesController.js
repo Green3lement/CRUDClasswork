10aa2c7fcbefdbc47d99e775f68cb35effd6e26a
Kyle VanSteelandt <60204098+Green3lement@users.noreply.github.com>
2021-05-05 19:54:54 -0600

    Update coursesController.js

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,163 +1,173 @@
 "use strict";
 
-const Course = require("../models/course");
-httpStatus = require("http-status-codes");
-User = require("../models/user"),
-    getCourseParams = body => {
-        return{
-            title: body.title,
-            description: body.description,
-            maxStudents: body.maxStudents,
-            cost: body.cost
-        };
-    };
-
+const Course = require("../models/course"),
+  httpStatus = require("http-status-codes"),
+  User = require("../models/user");
 
 module.exports = {
-    index: (req, res, next) => {
-        Course.find()
-        .then(courses => {
-            res.locals.courses = courses;
-            next()
-        })
-        .catch((error) => {
-            console.log(`Error fetching courses data: $error.message`);
-            next(error);
-        })
-    },
-    indexView: (req, res) => {
-        res.render("/courses/index");
-    },
-    new: (req, res) => {
-        res.render("/courses/new");
-    },
-    create: (req, res, next) => {
-        let newCourse = new Course({
-            title: req.body.title,
-            description: req.body.description,
-            maxStudent: req.body.maxStudent,
-            cost: req.body.cost
-        });
-        courses.create(newCourse)
-        .then(courses =>{
-            res.locals.courses = courses;
-            res.locals.redirect = "/courses";
-            next();
-        })
-        .catch((error) => {
-            console.log(`Error saving course: ${error.message}`);
-        })
-    },
-    redirectView: (req, res, next) =>{
-        let redirectPath = res.locals.redirect;
-        if(redirectPath != undefined) res.redirect(redirectPath);
-        else next();
-    },
-    show: (req, res, next) => {
-        let coursesId = req.params.id;
-        Course.findById(coursesId)
-        .then(courses => {
-            res.locals.courses = courses;
-            next();
-        })
-    },
-    showView: (req, res, next) =>{
-        res.render(courses/show);
-    },
-    edit: (req, res, next) => {
-        let coursesId = req.params.id;
-        Course.findById(coursesId)
-        .then(courses => {
-            res.redirect("/courses/edit",{courses: courses});
-        })
-    },
-    update: (req, res, next) => {
-        let coursesId = req.params.id;
-        let updatedCourse = new Course({
-            title: req.body.title,
-            description: req.body.description,
-            maxStudent: req.body.maxStudent,
-            cost: req.body.cost
-        });
+  index: (req, res, next) => {
+    Course.find({})
+      .then(courses => {
+        res.locals.courses = courses;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching courses: ${error.message}`);
+        next(error);
+      });
+  },
+  indexView: (req, res) => {
+    res.render("courses/index");
+  },
+  new: (req, res) => {
+    res.render("courses/new");
+  },
 
-        Course.findByIdAndUpdate(coursesId, updatedCourse)
-        .then(courses => {
-            res.locals.courses = courses;
-            res.locals.redirect = `/courses/${courses._id}`;
-            next();
-        })
-        .catch((error) => {
-            console.log(`Error fetching courses by ID: ${error.message}`);
-            next(error);
-        })
-    },
-    delete: (req, res, next) => {
-        let coursesId = req.params.id;
-        Course.findByIdAndRemove(coursesId)
-        .then(courses => {
-            res.locals.courses = courses;
-            res.locals.redirect = `/courses/`;
-            next();
-        })
-        .catch((error) => {
-            console.log(`Error fetching courses by ID: ${error.message}`);
-            next(error);
-        })
-    },
-    filterUserCourses: (req, res, next) => {
-        let currentUser = res.locals.currentUser;
-        if (currentUser) {
-          let mappedCourses = res.locals.courses.map(course => {
-            let userJoined = currentUser.courses.some(userCourse => {
-              return userCourse.equals(course._id);
-            });
-            return Object.assign(course.toObject(), { joined: userJoined });
-          });
-          res.locals.courses = mappedCourses;
-          next();
-        } else {
-          next();
-        }
-      },
-      respondJSON: (req, res) => {
-        res.json({
-          status: httpStatus.OK,
-          data: res.locals
+  create: (req, res, next) => {
+    let courseParams = {
+      title: req.body.title,
+      description: req.body.description,
+      items: [req.body.items.split(",")],
+      zipCode: req.body.zipCode
+    };
+    Course.create(courseParams)
+      .then(course => {
+        res.locals.redirect = "/courses";
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error saving course: ${error.message}`);
+        next(error);
+      });
+  },
+
+  show: (req, res, next) => {
+    let courseId = req.params.id;
+    Course.findById(courseId)
+      .then(course => {
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  showView: (req, res) => {
+    res.render("courses/show");
+  },
+
+  edit: (req, res, next) => {
+    let courseId = req.params.id;
+    Course.findById(courseId)
+      .then(course => {
+        res.render("courses/edit", {
+          course: course
         });
-      },
-      errorJSON: (error, req, res, next) => {
-        let errorObject;
-        if (error) {
-          errorObject = {
-            status: httpStatus.INTERNAL_SERVER_ERROR,
-            message: error.message
-          };
-        } else {
-          errorObject = {
-            status: httpStatus.INTERNAL_SERVER_ERROR,
-            message: "Unknown Error."
-          };
+      })
+      .catch(error => {
+        console.log(`Error fetching course by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  update: (req, res, next) => {
+    let courseId = req.params.id,
+      courseParams = {
+        title: req.body.title,
+        description: req.body.description,
+        items: [req.body.items.split(",")],
+        zipCode: req.body.zipCode
+      };
+
+    Course.findByIdAndUpdate(courseId, {
+      $set: courseParams
+    })
+      .then(course => {
+        res.locals.redirect = `/courses/${courseId}`;
+        res.locals.course = course;
+        next();
+      })
+      .catch(error => {
+        console.log(`Error updating course by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  delete: (req, res, next) => {
+    let courseId = req.params.id;
+    Course.findByIdAndRemove(courseId)
+      .then(() => {
+        res.locals.redirect = "/courses";
+        next();
+      })
+      .catch(error => {
+        console.log(`Error deleting course by ID: ${error.message}`);
+        next();
+      });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath !== undefined) res.redirect(redirectPath);
+    else next();
+  },
+  respondJSON: (req, res) => {
+    res.json({
+      status: httpStatus.OK,
+      data: res.locals
+    });
+  },
+  errorJSON: (error, req, res, next) => {
+    let errorObject;
+    if (error) {
+      errorObject = {
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message
+      };
+    } else {
+      errorObject = {
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: "Unknown Error."
+      };
+    }
+    res.json(errorObject);
+  },
+  join: (req, res, next) => {
+    let courseId = req.params.id,
+      currentUser = req.user;
+    if (currentUser) {
+      User.findByIdAndUpdate(currentUser, {
+        $addToSet: {
+          courses: courseId
         }
-        res.json(errorObject);
-      },
-      join: (req, res, next) => {
-        let courseId = req.params.id,
-          currentUser = req.user;
-        if (currentUser) {
-          User.findByIdAndUpdate(currentUser, {
-            $addToSet: {
-              courses: courseId
-            }
-          })
-            .then(() => {
-              res.locals.success = true;
-              next();
-            })
-            .catch(error => {
-              next(error);
-            });
-        } else {
-          next(new Error("User must log in."));
-        }
-      }
+      })
+        .then(() => {
+          res.locals.success = true;
+          next();
+        })
+        .catch(error => {
+          next(error);
+        });
+    } else {
+      next(new Error("User must log in."));
+    }
+  },
+  filterUserCourses: (req, res, next) => {
+    let currentUser = res.locals.currentUser;
+    if (currentUser) {
+      let mappedCourses = res.locals.courses.map(course => {
+        let userJoined = currentUser.courses.some(userCourse => {
+          return userCourse.equals(course._id);
+        });
+        return Object.assign(course.toObject(), { joined: userJoined });
+      });
+      res.locals.courses = mappedCourses;
+      next();
+    } else {
+      next();
+    }
+  }
 };
