542633085ea5f8694d6fba9b20546d635eaf368e
Kyle VanSteelandt <60204098+Green3lement@users.noreply.github.com>
2021-05-05 19:43:29 -0600

    deploy

--- models/course.js
+++ models/course.js
@@ -1,30 +1,23 @@
-const mongoose = require("mongoose"),
-{ Schema } = require("mongoose"),
-courseSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        maxStudents: {
-            type: Number,
-            default: 0,
-            min = [0, "Course can not have a negative number of Students."]
-        },
-        cost: {
-            type: Number,
-            default: 0,
-            min = [0, "Costs can not have a negative value."]
-        }
-    },
-    {
-        timestamp: true
-    }
-)
+"use strict";
+
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  items: [],
+  zipCode: {
+    type: Number,
+    min: [10000, "Zip code too short"],
+    max: 99999
+  }
+});
 
 module.exports = mongoose.model("Course", courseSchema);
