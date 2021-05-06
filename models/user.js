1cba2a92ca9ffbcb81292f283960d6a8db8e6105
Kyle VanSteelandt <60204098+Green3lement@users.noreply.github.com>
2021-05-05 19:46:08 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,71 +1,79 @@
 "use strict";
-const passportLocalMongoose = require("passport-local-mongoose");
 
 const mongoose = require("mongoose"),
-{ Schema } = require("mongoose"),
-Subscriber = require("./subscriber");
-
-userSchema = new Schema(
+  { Schema } = mongoose,
+  Subscriber = require("./subscriber"),
+  bcrypt = require("bcrypt-nodejs"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  randToken = require("rand-token"),
+  userSchema = new Schema(
     {
-        name: {
-            first: {
-            type: String,
-            required: true,
-            },
-            last: {
-                type: String,
-                required: true
-            }
+      name: {
+        first: {
+          type: String,
+          trim: true
         },
-        email:{
-            type: String,
-            required: true,
-            unique: true
-        },
-        zipCode: {
-            type: Number,
-            min: [10000, "Zip code too short"],
-            max: 99999
-        },
-        courses: [{
-            type: Schema.Types.ObjectId, ref: "Course"
-        }],
-        subscribedAccount: {
-            type: Schema.Types.ObjectId, ref: "Subscriber"
+        last: {
+          type: String,
+          trim: true
         }
+      },
+      email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+      },
+      // apiToken: {
+      //   type: String
+      // },
+      zipCode: {
+        type: Number,
+        min: [1000, "Zip code too short"],
+        max: 99999
+      },
+      courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+      subscribedAccount: {
+        type: Schema.Types.ObjectId,
+        ref: "Subscriber"
+      }
     },
     {
-        timestamp: true
+      timestamps: true
     }
-)
+  );
 
-userSchema.virtual("fullName").get(function () {
-    return `${this.name.first} ${this.name.last}`;
+userSchema.virtual("fullName").get(function() {
+  return `${this.name.first} ${this.name.last}`;
 });
 
 userSchema.pre("save", function(next) {
-    let user = this;
-    if(user.subscribedAccount == undefined){
-        Subscriber.findOne({
-            email: user.email
-        })
-        .then(subscriber => {
-            user.subscribedAccount = subscriber;
-            next();
-        })
-        .catch(error => {
-            console.log(`error in associating subscriber: ${error.message}`)
-            next(error);
-        })
-    }
-    else {
+  let user = this;
+  if (user.subscribedAccount === undefined) {
+    Subscriber.findOne({
+      email: user.email
+    })
+      .then(subscriber => {
+        user.subscribedAccount = subscriber;
         next();
-    }
+      })
+      .catch(error => {
+        console.log(`Error in connecting subscriber:${error.message}`);
+        next(error);
+      });
+  } else {
+    next();
+  }
 });
+
+// userSchema.pre("save", function(next) {
+//   let user = this;
+//   if (!user.apiToken) user.apiToken = randToken.generate(16);
+//   next();
+// });
 
 userSchema.plugin(passportLocalMongoose, {
-    usernameField: "email"
+  usernameField: "email"
 });
-
 
 module.exports = mongoose.model("User", userSchema);
